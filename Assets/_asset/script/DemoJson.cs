8ba63e974b1307331c688a1f394ef12306525378
kanne <doantrungkien18@gmail.com>
2024-12-27 21:19:23 +0700

    update 27/12

--- Assets/_asset/script/DemoJson.cs
+++ Assets/_asset/script/DemoJson.cs
@@ -22,8 +22,61 @@ public class DemoJson : MonoBehaviour
     public void LoadStar()
     {
         demoVectorString = PlayerPrefs.GetString(VECTOR_KEY, string.Empty);
-        demoVectorString = string.IsNullOrEmpty(demoVectorString)? JsonUtility.ToJson(Vector3.zero) : demoVectorString;
+        demoVectorString = string.IsNullOrEmpty(demoVectorString) ? JsonUtility.ToJson(Vector3.zero) : demoVectorString;
 
         demoVector = JsonUtility.FromJson<Vector3>(demoVectorString);
     }
+    
+
+
+    public GameData gameData;
+    public string demoGameDataString;
+    private const string GAME_DATA_KEY = "GAME_DATA_KEY";
+
+    [ContextMenu("Save Data")]
+    public void SaveData()
+    {
+        demoGameDataString = JsonUtility.ToJson(gameData);
+        PlayerPrefs.SetString(GAME_DATA_KEY, demoGameDataString);
+        PlayerPrefs.Save();
+    }
+
+    [ContextMenu("Load Data")]
+    public void LoadData()
+    {
+        demoGameDataString = PlayerPrefs.GetString(GAME_DATA_KEY, string.Empty);
+        demoGameDataString = string.IsNullOrEmpty(demoGameDataString) ? JsonUtility.ToJson(gameData) : demoGameDataString;
+
+        gameData = JsonUtility.FromJson<GameData>(demoGameDataString);
+    }
+}
+[Serializable]
+public class GameData
+{
+
+    public int gold;
+    public UserInfo userinfo;
+    public DateTime firstPlay;
+    public string version;
+    public List<Session> sessions;
+
+}
+[Serializable]
+public class Session
+{
+    public DateTime startTime;
+    public DateTime endTime;
+    public List<int> scores;
+}
+[Serializable]
+public class UserInfo
+{
+    public string username;
+    public int age;
+    public DateTime dateOfBirth;
+    public Sex sex;
+}
+public enum Sex
+{
+    Male, Female
 }
